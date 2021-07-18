b5f9e7545f044e06c36933142537e947c3771c93
valharault <valentinharault1@gmail.com>
2021-07-18 16:44:41 +0200

    refacto logging & handle logging user

--- client/src/views/Body.jsx
+++ client/src/views/Body.jsx
@@ -1,21 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {Container} from "react-bootstrap";
-import LoginForm from './../components/security/merchand/LoginForm';
-import AdminLoginForm from './../components/security/admin/AdminLoginForm';
-import RegisterForm from './../components/security/merchand/RegisterForm';
 
 export default function Body() {
-    const [user, setUser] = useState(null)
-
-    useEffect(function () {
-       setUser(false)
-    }, [])
-
-    if (user === null) {
-        return null;
-    }
-
     return (
-        user ? <Container /> : <AdminLoginForm onConnect={setUser} />
+        <Container>
+            Bienvenue sur notre Application React / NodeJS
+        </Container>
     );
 }
