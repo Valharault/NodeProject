d755269b8258865c732c19b5d3bfc406f3ffcee8
Mohand AIT AMARA <mohand.aitamara@faurecia.com>
2021-07-24 01:00:02 +0200

    mise en place du PSP

--- psp/functions/functions.js
+++
@@ -1,8 +0,0 @@
-exports.isEmpty = function isEmpty(value) {
-    if (typeof value === "undefined" && !value) return true;
-    else {
-      if (value != "" && value != " ") return false;
-      else return true;
-    }
-    return false;
-  };
--- psp/index.js
+++ psp/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const Payments = require("./routes/payments/routes");
+const http = require('http');
 
 const app = express();
 
@@ -8,7 +8,38 @@ app.use(express.urlencoded());
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/payments", Payments);
+const data = new TextEncoder().encode(
+    "Confirmed"
+);
 
+const options = {
+    hostname: 'localhost',
+    port: 4000,
+    path: '/api/transactions/1',
+    methode: 'PUT',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
+app.use("/api/capture_refund/1", (req, res) => {
+    res.status(202);
+    res .send();
+    setTimeout(() => {
+        const requ = http.request(options, resp => {
+            resp.on('data', d => {
+                process.stdout.wrrite(d)
+            })
+        })
+    
+        requ.on('error', error => {
+            console.error(error)
+        })
+    
+        requ.write(data)
+        requ.end()
+    }, 10000);
+    
+});
 
 app.listen(process.env.PORT || 5000, () => console.log("server is listening"));
